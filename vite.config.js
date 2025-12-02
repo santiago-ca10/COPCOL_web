1d691a3c479e6da8ab7e2bcb312c7802e1e5fd09
SCA10 <santycardal2019@gmail.com>
2025-12-01 22:02:09 -0500

    cambios

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/COPCOL_web/',
   plugins: [react()],
-  base: '/COPCOL_web/'    
 })
